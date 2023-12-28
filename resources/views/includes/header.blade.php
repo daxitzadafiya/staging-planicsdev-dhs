32779f8275ae24a62ed4b6a6bfc49f119ba2fb17
Daxit Zadafiya <daxitzadafiya.work@gmail.com>
2023-12-28 11:17:38 +0530

    Worked on css changes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,8 +1,7 @@
 <div class="top-bar-boxed h-[70px] md:h-[65px] z-[51] border-b border-white/[0.08] md:mt-12 mt-0  -mx-3 sm:-mx-8 md:-mx-0 px-3 md:border-b-0 relative md:fixed md:inset-x-0 md:top-0 sm:px-8 md:px-10 md:pt-10 md:bg-gradient-to-b md:from-slate-100 md:to-transparent dark:md:from-darkmode-700">
-    <div class="h-full flex items-center">
+    <div class="h-full flex items-center relative">
         <a href="{{ route('dashboard') }}" class="logo -intro-x hidden md:flex xl:w-[180px] block">
-            <img alt="Planics Dev" class="logo__image w-6" src="{{ asset('frontend/assets/img/fav-icon.png') }}">
-            <span class="logo__text text-white text-lg ml-3"> {{ env('APP_NAME') }} </span>
+            <img alt="Planics Dev" class="logo__image header-logo" src="{{ asset('frontend/assets/img/footer-logo.png') }}">
         </a>
         <nav aria-label="breadcrumb" class="-intro-x h-[45px] mr-auto">
             <ol class="breadcrumb breadcrumb-light">
